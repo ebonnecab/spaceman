fbd8f3235289a835eae8dbb984b6ab581638d430
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-11-05 14:12:15 -0800

    functions added

--- spaceman.py
+++ spaceman.py
@@ -1,68 +1,3 @@
-# import random
-
-# def load_word():
-#    f = open('words.txt', 'r')
-#    words_list = f.readlines()
-#    f.close()
-
-#    words_list = words_list[0].split(' ')
-#    secret_word = random.choice(words_list)
-#    return secret_word
-
-# def is_word_guessed(secret_word, letters_guessed):
-#     '''
-#     secretWord: string, the random word the user is trying to guess.  This is selected on line 9.
-#     lettersGuessed: list of letters that have been guessed so far.
-#     returns: boolean, True only if all the letters of secretWord are in lettersGuessed;
-#       False otherwise
-#     '''
-
-#     # FILL IN YOUR CODE HERE...
-
-# def get_guessed_word(secret_word, letters_guessed):
-#     '''
-#     secretWord: string, the random word the user is trying to guess.  This is selected on line 9.
-#     lettersGuessed: list of letters that have been guessed so far.
-#     returns: string, of letters and underscores.  For letters in the word that the user has
-#     guessed correctly, the string should contain the letter at the correct position.  For letters
-#     in the word that the user has not yet guessed, shown an _ (underscore) instead.
-#     '''
-#     # FILL IN YOUR CODE HERE...
-
-
-
-
-# def get_available_letters(letters_guessed):
-#     '''
-#     lettersGuessed: list of letters that have been guessed so far
-#     returns: string, comprised of letters that represents what letters have not
-#       yet been guessed.
-#     '''
-
-
-
-
-# def spaceman(secret_word):
-#     '''
-#     secretWord: string, the secret word to guess.
-#     Starts up a game of Spaceman in the command line.
-#     * At the start of the game, let the user know how many
-#       letters the secretWord contains.
-#     * Ask the user to guess one letter per round.
-#     * The user should receive feedback immediately after each guess
-#       about whether their guess appears in the computer's word.
-#     * After each round, you should also display to the user the
-#       partially guessed word so far, as well as letters that the
-#       user has not yet guessed.
-#     '''
-#     # FILL IN YOUR CODE HERE...
-
-
-# #
-# # secret_word = load_word()
-# # spaceman(load_word())
-
-
 #importing time module
 import time
 
@@ -77,43 +12,85 @@ def user_name():
   name = input("What is your name?: ")
   print("Hello {}! Are you ready to play Spaceman?".format(name))
 
-
+#dramatic pause before the game begins
 time.sleep(1)
 
+#the word is randomly chosen and loaded from the file
 def load_word():
   with open("words.txt","r") as file:
     words = file.readlines()
     word = random.choice(words)
     return word
 
-def guess_letter():
+def user_guess():
   letter = input("Guess a letter!: ")
   if len(letter) > 1:
     print("Not a valid guess! Try again")
-  
+    user_guess()
   elif not letter.isalpha():
     print("Not a valid guess! Try again")
-
+    user_guess()
   elif letter == "":
     print("Not a valid guess! Try again")
-  
-  elif letter == " ":
-    print("Not a valid guess! Try again")
+    user_guess()
 
   return letter
-  
+
 def letter_dashes():
   word = load_word()
-  dashes = list("_" * (len(word) - 1))
+  dashes = " _ " * (len(word))
   return dashes
+
+def get_guess():
+  word = load_word()
+  dashes = letter_dashes()
+  guesses_left = 7
+  while guesses_left > -1 and dashes != word:
+    print(dashes)
+    print(str(guesses_left))
+    guess = user_guess()
+    if guess in word:
+      print ("That letter is in the word!")
+      dashes = update_dashes()
+    else:
+      print("That letter is incorrect!")
+      guesses_left -= 1
+    
+    if guesses_left < 0:
+      print ("You lose! The word was: " + str(word))
+    
+    else:
+      print ("Good Job!")
+def update_dashes ():
+  word = load_word()
+  guess = user_guess()
+  dash = letter_dashes()
+  result = ""
+
+  for i in range (len(word)):
+    if word[i] == guess:
+      result = result + guess
+
+    else:
+      result = result + dash[i]
+  
+  return result
+
+  
+  
+
+ 
 
 
 
 
 def main():
-  # user_name()
-  print(letter_dashes())
-  # guess_letter()
+  user_name()
+  # load_word()
+  # letter_dashes()
+  get_guess()
+  update_dashes()
+  
 
 
 
