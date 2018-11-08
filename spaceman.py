1a5ec764ed936e76da65e9757965892777d4badf
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-11-08 08:20:35 -0800

    increased functionality

--- spaceman.py
+++ spaceman.py
@@ -1,102 +1,113 @@
-#importing time module
-import time
-
-#importing random
+#importing random and time module
+from time import sleep
 import random
 
 #list of guessed letters
 guessed_letters =[]
 
 #asks user their name
-def user_name():
+def user_rules():
   name = input("What is your name?: ")
   print("Hello {}! Are you ready to play Spaceman?".format(name))
+  print("There is a secret word.\n You get seven guesses to figure out the letters in the word before you lose. \nGood luck!")
+  sleep(2)
 
-#dramatic pause before the game begins
-time.sleep(1)
-
-#the word is randomly chosen and loaded from the file
+#the word is randomly chosen and loaded from the dictionary
 def load_word():
-  with open("words.txt","r") as file:
-    words = file.readlines()
-    word = random.choice(words)
-    return word
+  dictonary = {
+   "Colors": ["Blue", "Red", "White"],
+   "Animals": ["Dog", "Bird", "Lion"] 
+  }
+  categories = ["Colors", "Animals"]
+  categories_index = random.randint(0, 1)
+  word = random.randint(0, 2)
+  secret_word = dictonary[categories[categories_index]][word]
+  return secret_word
+
+secret_word = load_word().lower()
+
+def game_over(start_over):
+  answer = input("Game over! Would you like to play again? (Y/N): ").lower()
+  if answer == 'y':
+    start_over()
+  elif answer != 'y' or answer != 'n':
+    print("Invalid input")
+    game_over(start_over)
+  else:
+    print("See you next time!")
+    return False
 
 def user_guess():
-  letter = input("Guess a letter!: ")
-  if len(letter) > 1:
-    print("Not a valid guess! Try again")
-    user_guess()
-  elif not letter.isalpha():
-    print("Not a valid guess! Try again")
-    user_guess()
-  elif letter == "":
-    print("Not a valid guess! Try again")
-    user_guess()
-
-  return letter
+    answer = input("Guess a letter!: ")
+    if len(answer) > 1 or not answer.isalpha() or answer == "":
+        print("Not a valid guess! Try again")
+        user_guess()
+    else:
+        return answer
 
 def letter_dashes():
-  word = load_word()
-  dashes = " _ " * (len(word))
-  return dashes
+  dashes = "_" * (len(secret_word))
+  return list(dashes)
 
-def get_guess():
-  word = load_word()
-  dashes = letter_dashes()
+
+# guessed_letters = []
+def guessed_letters_list():
+  return "These are the letters you already guessed: {}".format(guessed_letters)
+
+def right_guess():
+  print("{} was in the word! +\n{}".format(user_guess(), guessed_letters_list()))
+
+  
+
+def win_guess():
+  print("Good job! The word was {}".format(secret_word))
+
+def wrong_guess():
+  print("{} is wrong. Try again.\n{}".format(user_guess(), guessed_letters_list()))
+
+# def guess_message():
+  # print("You have already guessed{}. Try again\n{}".format(guessed_letters_list(), )
+
+def game():
+  user_rules()
   guesses_left = 7
-  while guesses_left > -1 and dashes != word:
-    print(dashes)
-    print(str(guesses_left))
-    guess = user_guess()
-    if guess in word:
-      print ("That letter is in the word!")
-      dashes = update_dashes()
-    else:
-      print("That letter is incorrect!")
+  dashes = letter_dashes()
+  print(dashes)
+
+  correct_guess = []
+
+  while guesses_left > -1 and dashes != secret_word:
+    letter = user_guess()
+    if guesses_left == 0:
+      game_over(game)
+
+    if letter in guessed_letters:
       guesses_left -= 1
+      guessed_letters_list()
     
-    if guesses_left < 0:
-      print ("You lose! The word was: " + str(word))
+    elif letter in secret_word:
+      right_guess()
+      dashes[secret_word.index(letter)] = letter
+      print(dashes)
+      guessed_letters_list()
     
-    else:
-      print ("Good Job!")
-def update_dashes ():
-  word = load_word()
-  guess = user_guess()
-  dash = letter_dashes()
-  result = ""
-
-  for i in range (len(word)):
-    if word[i] == guess:
-      result = result + guess
+    elif correct_guess in guessed_letters:
+      win_guess()
+    
 
     else:
-      result = result + dash[i]
-  
-  return result
+      guesses_left -= 1
+      guessed_letters.append(letter)
+      wrong_guess()
 
-  
+
   
 
  
 
 
 
+def test():
+  game()
 
-def main():
-  user_name()
-  # load_word()
-  # letter_dashes()
-  get_guess()
-  update_dashes()
-  
-
-
-
-# #test rounds
-# for _ in list(range(1,5)):
-#   letter_dashes()
-#   guess_letter()
-
-main()
+test()
